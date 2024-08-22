54a85c6777578c73ebe7b679098cb364910fa88f
ycb-web <1090489064@qq.com>
2024-08-22 21:37:45 +0800

    新增 新增笔记

--- src/learn-page/memo.tsx
+++ src/learn-page/memo.tsx
@@ -13,7 +13,7 @@ const MemSon: FC<{ nums: number[]; onHandleClick: () => void }> = memo(
 
 // 2. 传递一个复杂类型的prop 比较的是新值和旧的引用是否相等 当父组件的函数重新执行时，实际上形成的应用变了
 
-// 3.保证引用的稳定 使用useMemo缓存值，使用useCallback缓存函数引用
+// 3. 保证引用的稳定 使用useMemo缓存值，使用useCallback缓存函数引用
 function App() {
   const [count, setCount] = useState(0);
   console.log("App --->>> render");
--- src/learn-page/router.tsx
+++ src/learn-page/router.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from "react";
-import { Link, useNavigate, NavLink } from "react-router-dom";
+import React, { FC, useState } from "react";
+import { Link, useNavigate, NavLink, Navigate } from "react-router-dom";
 const routerFC: FC = () => {
-  const Navigate = useNavigate();
+  const NavigateFun = useNavigate();
   const goHome = () => {
-    Navigate("/home?id=123");
+    NavigateFun("/home?id=123");
   };
+  const [isLogin, setIsLogin] = useState(false);
   return (
     <>
       <div>router page</div>
@@ -14,6 +15,9 @@ const routerFC: FC = () => {
       <div>-----------------</div>
       {/* 命令式跳转 */}
       <button onClick={goHome}>go to home page</button>
+      {/* 只要看到这个组件，就执行跳转 */}
+      <div>-----------------</div>
+      {!isLogin ? <Navigate to="/login" /> : 'login success'}
     </>
   );
 };
